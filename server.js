07ba61fb255acc99096ad21eb671d9a3cf84d622
technicalsushantoffical <126672750+technicalsushantoffical@users.noreply.github.com>
2023-03-01 17:06:22 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const server = express();
+
+server.all(`/`, (req, res) => {
+    res.send(`Please connect me to a hosting website in-order to work 24/7.`);
+});
+
+function keepAlive() {
+    server.listen(3000, () => {
+        console.log(`24/7 Activation Complete`);
+    });
+}
+
+module.exports = keepAlive;
